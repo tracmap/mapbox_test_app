23f1fb79b62a2b7e3c3f8711e35148e77013f6df
dmcauslan <david.mcauslan@tracmap.com>
2021-05-08 10:54:36 +1200

    Add code to simulate panning the map rapidly to make it easier to cause database corruption errors

--- app/src/main/java/com/tracmap/mapboxtestapp/MainActivity.kt
+++ app/src/main/java/com/tracmap/mapboxtestapp/MainActivity.kt
@@ -2,7 +2,8 @@ package com.tracmap.mapboxtestapp
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import com.mapbox.geojson.Point.fromLngLat
+import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.MapView
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
@@ -28,20 +29,53 @@ class MainActivity : Activity(), CoroutineScope {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mapInstance.loadStyleUri(Style.SATELLITE)
-
         insertTestDataIntoDatabase()
+
+        mapInstance.loadStyleUri(Style.SATELLITE)
+        simulatePanningMap()
     }
 
     private fun insertTestDataIntoDatabase() {
         launch(Dispatchers.IO) {
             while (true) {
-                Log.d(LOG_TAG, "Inserting into database")
                 dbHelper.use {
                     insertOrThrow(TEST_TABLE_NAME, COLUMN_1 to UUID.randomUUID().toString())
                 }
                 delay(DATABASE_INSERT_DELAY_MS)
             }
         }
+    }
+
+    private fun simulatePanningMap() {
+        launch {
+            var lat = 10.0
+            var lon = -5.0
+            val deltaLat = 0.005
+            val deltaLon = 0.01
+
+            while (true) {
+                moveCamera(lat, lon)
+                lat += deltaLat
+                lon += deltaLon
+                /** Delay for 16ms to simulate screen running at 60fps */
+                delay(16)
+            }
+        }
+    }
+
+    private fun moveCamera(topLeftLat: Double, topLeftLon: Double) {
+        val bottomRightLat = topLeftLat + 0.1
+        val bottomRightLon = topLeftLon + 0.1
+
+        val cameraOptions = mapInstance.cameraForCoordinates(
+            listOf(fromLngLat(topLeftLon, topLeftLat), fromLngLat(bottomRightLon, bottomRightLat)),
+            EdgeInsets(0.0, 0.0, 0.0, 0.0),
+            0.0,
+            0.0
+        )
+
+        mapInstance.setCamera(cameraOptions)
+        /** Uncomment this line, and comment out the one above for testing with older mapbox versions */
+//        mapInstance.jumpTo(cameraOptions)
     }
 }
